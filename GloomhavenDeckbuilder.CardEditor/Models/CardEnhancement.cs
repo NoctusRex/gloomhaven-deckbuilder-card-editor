8abfe1607c0a4fc4bcffebcc3deb9375b34556f2
Noctus Rex <noxjudex@gmail.com>
2022-05-09 18:59:07 +0200

    Bugfix Json Export

--- GloomhavenDeckbuilder.CardEditor/Models/CardEnhancement.cs
+++ GloomhavenDeckbuilder.CardEditor/Models/CardEnhancement.cs
@@ -1,4 +1,6 @@
-﻿namespace GloomhavenDeckbuilder.CardEditor.Models
+﻿using Newtonsoft.Json;
+
+namespace GloomhavenDeckbuilder.CardEditor.Models
 {
     public enum AbilityLine
     {
@@ -12,13 +14,25 @@
     /// </summary>
     public class CardEnhancement
     {
+        [JsonProperty("x")]
         public int X { get; set; }
+
+        [JsonProperty("y")]
         public int Y { get; set; }
 
+        [JsonProperty("abilityLine")]
         public AbilityLine AbilityLine { get; set; } = AbilityLine.Main;
+
+        [JsonProperty("isNumeric")]
         public bool IsNumeric { get; set; } = false;
+
+        [JsonProperty("canTargetEnemies")]
         public bool CanTargetEnemies { get; set; } = false;
+
+        [JsonProperty("canTargetAllies")]
         public bool CanTargetAllies { get; set; } = false;
+
+        [JsonProperty("isMovement")]
         public bool IsMovement { get; set; } = false;
     }
 }
